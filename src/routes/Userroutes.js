aebd5bb19e2cee39d7fcd4a0c5c6e8aa2243deec
maulik pagada <pagadamaulik4@gmail.com>
2023-07-17 13:24:56 +0530

    07_17_redux_counter

--- src/routes/Userroutes.js
+++ src/routes/Userroutes.js
@@ -14,36 +14,44 @@ import Auth from '../user/container/Auth/Auth';
 import FromValidation from '../user/container/Form_validation/FromValidation';
 import Medicine from '../user/container/Medicine/Medicine';
 import PrivateRoute from './PrivateRoute';
+import Counter from '../user/container/Counter/Counter';
+import { configureStore } from '../redux/store';
+import { Provider } from 'react-redux';
 // import PrivateRoute from '../utils/PrivateRoute';
 
 function Userroutes(props) {
+
+    let store = configureStore()
     return (
         <>
-            <Header />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/appointment' element={<Appointment />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/departments' element={<Departments />} />
-                <Route path='/doctors' element={<Doctors />} />
-                <Route path='/doctor/:id' element={<Doctor />} />
-                <Route path='/doctor/vistingdoctor' element={<VistingDoctor />} />
-                <Route path='*' element={<NotFound />} />
-
-                <Route path='/formvalidation' element={<FromValidation />} />
-
-                <Route path='/doctor'>
+            <Provider store={store}>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/appointment' element={<Appointment />} />
+                    <Route path='/contact' element={<Contact />} />
+                    <Route path='/departments' element={<Departments />} />
+                    <Route path='/doctors' element={<Doctors />} />
                     <Route path='/doctor/:id' element={<Doctor />} />
                     <Route path='/doctor/vistingdoctor' element={<VistingDoctor />} />
-                </Route>
+                    <Route path='*' element={<NotFound />} />
 
-                <Route element={<PrivateRoute />}>
-                    <Route path='/medicine' element={<Medicine />} />
-                </Route>
-                <Route path='/auth' element={<Auth />} />
-            </Routes>
-            <Footer />
+                    <Route path='/formvalidation' element={<FromValidation />} />
+
+                    <Route path='/doctor'>
+                        <Route path='/doctor/:id' element={<Doctor />} />
+                        <Route path='/doctor/vistingdoctor' element={<VistingDoctor />} />
+                    </Route>
+
+                    <Route element={<PrivateRoute />}>
+                        <Route path='/medicine' element={<Medicine />} />
+                    </Route>
+                    <Route path='/auth' element={<Auth />} />
+                    <Route path='/counter' element={<Counter />} />
+                </Routes>
+                <Footer />
+            </Provider>
         </>
     );
 }
---
+++ src/user/container/Counter/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../../../redux/action/counter.action';
+
+function Counter(props) {
+    const dispatch = useDispatch()
+    const couterData = useSelector(state=>state.counter)
+
+    const handleInc = () => {
+        dispatch(increment())
+    }
+
+    const handleDec = () => {
+        dispatch(decrement())
+    }
+
+    return (
+        <div>
+            <button onClick={() => handleInc()}>+</button>
+            <span>{couterData.count}</span>
+            <button onClick={() => handleDec()}>-</button>
+        </div>
+    );
+}
+
+export default Counter;
