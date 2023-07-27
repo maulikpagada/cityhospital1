2aae6f0878b8db788b1f79d9ac99c73a1257a708
maulik pagada <pagadamaulik4@gmail.com>
2023-07-27 07:39:47 +0530

    26_07_redux_fetch_api_total_sum_cart

--- src/user/container/Cart/Cart.js
+++ src/user/container/Cart/Cart.js
@@ -34,55 +34,53 @@ function Cart(props) {
     }
 
     return (
-
-        <section id="medicines" className="medicines">
+        <section id="medicine" className="medicine">
             <div className="container">
                 <div className="section-title">
-                    <h2>cart</h2>
-
+                    <h2>Cart</h2>
                 </div>
-            </div>
-            <div className="container">
-                <div className="card mb-3">
-                    {
-                        cartitems.map((a,i) => {
-                            return (
 
+                {
+                    cartitems.map((c) => {
+                        return (
+                            <div className="card mb-3">
                                 <div className="card-body">
                                     <div className="d-flex justify-content-between">
                                         <div className="d-flex flex-row align-items-center">
-
                                             <div className="ms-3">
-                                                <h5>{a.name}</h5>
-                                                <p className="small mb-0">{a.desc.substring(0, 20)}</p>
+                                                <h5>I{c.name}</h5>
+                                                <p className="small mb-0">{c.desc.substring(0, 50)}...</p>
                                             </div>
                                         </div>
                                         <div className="d-flex flex-row align-items-center">
-                                            <div style={{ width: 50 }}>
-                                                <button onClick={() => handleDec(a.pid)}>-</button>
-                                                <h5 className="fw-normal mb-0">{a.qty}</h5>
-                                                <button onClick={() => handleInc(a.pid)}>+</button>
+                                            <div className="d-flex mx-5" style={{ width: 150 }}>
+                                                <button className="btn btn-link px-2" onClick={() => handleDec(c.pid)}>
+                                                    <i className="fas fa-minus" />
+                                                </button>
+                                                <input id="form1" min={1} name="quantity" value={c.qty} type="text" className="form-control form-control-sm" />
+                                                <button className="btn btn-link px-2" onClick={() => handleInc(c.pid)}>
+                                                    <i className="fas fa-plus" />
+                                                </button>
                                             </div>
-                                            <div style={{ width: 80 }}>
-                                                <h5 className="mb-0">{a.qty * a.price}</h5>
+                                            <div style={{ width: 130 }}>
+                                                <h5 className="mb-0">Rs.{c.price * c.qty}</h5>
                                             </div>
-                                            <a href="#!" style={{ color: '#cecece' }} onClick={() => handleremove(a.pid)}><i className="fas fa-trash-alt" /></a>
+                                            <a style={{ color: '#cecece' }}><i className="fas fa-trash-alt" onClick={() => handleremove(c.pid)} /></a>
                                         </div>
-
                                     </div>
                                 </div>
-                            )
-                        })
-                    }
-                    <div className=" card-body card mb-3 ms-3 d-flex justify-content-between d-flex flex-row align-items-center">
-                        Total
-                        <div style={{ width: 80 }}>
-                            <h5 className="mb-0">{totalPrice}</h5>
-                        </div>
+                            </div>
+                        )
+                    })
+                }
+
+                <div className=" card-body card mb-3 ms-3 d-flex justify-content-between d-flex flex-row align-items-center">
+                    Total Amount Is:-
+                    <div style={{ width: 80 }}>
+                        <h5 className="mb-0">{totalPrice}</h5>
                     </div>
                 </div>
             </div>
-
         </section>
     );
 }
